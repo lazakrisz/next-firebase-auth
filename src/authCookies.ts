42ea7db19c8e90febc8537cd95814328d0b2bb04
lazakrisz <lezer11@icloud.com>
2025-05-14 16:07:24 +0200

    refactor: simplify cookie name retrieval for merged cookies

--- src/authCookies.ts
+++ src/authCookies.ts
@@ -1,9 +1,13 @@
 import { getConfig } from 'src/config'
 
 const getBaseCookieName = () => getConfig().cookies.name
+const isMergedCookie = () => getConfig().cookies.merged
 
 export const getUserCookieName = () => {
   const baseAuthCookieName = getBaseCookieName()
+  if (isMergedCookie()) {
+    return baseAuthCookieName
+  }
   return `${baseAuthCookieName}.AuthUser` // do not modify
 }
 
@@ -11,6 +15,9 @@ export const getUserSigCookieName = () => `${getUserCookieName()}.sig`
 
 export const getUserTokensCookieName = () => {
   const baseAuthCookieName = getBaseCookieName()
+  if (isMergedCookie()) {
+    return baseAuthCookieName
+  }
   return `${baseAuthCookieName}.AuthUserTokens` // do not modify
 }
 
